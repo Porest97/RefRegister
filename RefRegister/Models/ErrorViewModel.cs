59f987659520e0809e0b2c6063fb5ae3d37327b3
Per Orest <per.orest@outlook.com>
2019-02-13 13:26:03 +0100

    Add project files.

---
+++ RefRegister/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace RefRegister.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
